9b15724c242fdb5f46b9eae6baa6744554f0607d
Dorner Nic Alexander <nicalexander.dorner@students.bfh.ch>
2020-09-23 18:17:45 +0200

    changed parameters for generate table command

--- src/main/java/ch/nmbe/measure/GenerateTableCommand.java
+++ src/main/java/ch/nmbe/measure/GenerateTableCommand.java
@@ -6,10 +6,12 @@ import ij.gui.Overlay;
 import ij.gui.Roi;
 import ij.io.Opener;
 import io.scif.services.DatasetIOService;
+import net.imagej.ImageJ;
 import net.imagej.roi.ROIService;
 import org.scijava.command.Command;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
+import org.scijava.ui.UserInterface;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -31,8 +33,14 @@ public class GenerateTableCommand implements Command {
     @Parameter(style = "directory", label = "Image Folder", description = "Select the folder that contains the images you want to measure")
     private File imageFolder;
 
-    @Parameter(label = "Scale", description = "Scale of image in pixels per 1mm", min = "1")
-    private int scale;
+    @Parameter(choices = {"µm", "mm", "cm"}, style = "listBox", label = "Unit", description = "Unit of measurement")
+    private String unit;
+
+    @Parameter(label = "Distance", description = "Real distance in the specified unit", min = "1")
+    private int distance;
+
+    @Parameter(label = "Pixel", description = "Number of pixels for the specified distance", min = "1")
+    private int pixel;
 
     @Override
     public void run() {
@@ -42,19 +50,19 @@ public class GenerateTableCommand implements Command {
         File[] files = imageFolder.listFiles(MeasureService.tifFilter);
         for (File file : files) {
             String featureName = "";
-            String individual = "";
+            String specimen = "";
 
             Matcher matcher = MeasureService.fileNamePattern.matcher(file.getName());
             boolean isMatching = matcher.matches();
             if (isMatching) {
-                individual = matcher.group(1);
+                specimen = matcher.group(1);
                 featureName = matcher.group(3);
             }
 
-            if (!table.containsKey(individual)) {
-                table.put(individual, new HashMap<>());
+            if (!table.containsKey(specimen)) {
+                table.put(specimen, new HashMap<>());
             }
-            Map<String, Double> row = table.get(individual);
+            Map<String, Double> row = table.get(specimen);
 
             Opener opener = new Opener();
             ImagePlus imagePlus = opener.openTiff(file.getParent(), file.getName());
@@ -67,7 +75,7 @@ public class GenerateTableCommand implements Command {
                         if (!columnNames.contains(featureName)) {
                             columnNames.add(featureName);
                         }
-                        row.put(featureName, line.getRawLength() / this.scale);
+                        row.put(featureName, line.getRawLength() / this.pixel * this.distance);
                     }
                 }
             }
@@ -81,9 +89,9 @@ public class GenerateTableCommand implements Command {
         try {
             FileWriter writer = new FileWriter(outputFile, false);
 
-            StringBuilder header = new StringBuilder("Individual");
+            StringBuilder header = new StringBuilder("Specimen");
             for (String title : columnNames) {
-                header.append(",").append(title);
+                header.append(",").append(title).append(" ").append(this.unit);
             }
             header.append("\n");
             writer.write(header.toString());
@@ -96,7 +104,7 @@ public class GenerateTableCommand implements Command {
                     if (!column.containsKey(title)) {
                         continue;
                     }
-                    line.append(column.get(title)).append("mm");
+                    line.append(column.get(title));
                 }
                 line.append("\n");
                 writer.write(line.toString());
@@ -106,5 +114,13 @@ public class GenerateTableCommand implements Command {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    /** Tests the command. */
+    public static void main(final String... args) throws Exception {
+        // Launch ImageJ for test run.
+        final ImageJ ij = new ImageJ();
+        ij.launch(args);
+        ij.command().run(GenerateTableCommand.class, true);
     }
 }
