9b856746f1f0dc1d2c8ebae0482cbebe52c67756
Nic Dorner <nic.dorner@me.com>
2020-10-04 15:57:32 +0200

    correction command

---
+++ src/main/java/ch/nmbe/measure/CorrectionCommand.java
@@ -0,0 +1,34 @@
+package ch.nmbe.measure;
+
+import net.imagej.ImageJ;
+import org.scijava.command.Command;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+import java.io.File;
+import java.util.Arrays;
+
+@Plugin(type = Command.class, headless = true,
+        menuPath = "Analyze>Batch Measure>Generate Measure Table")
+public class CorrectionCommand implements Command {
+    @Parameter(style = "files,extensions:tiff",  label = "Files to correct", description = "Select all files you want to open again")
+    private File[] filesToCorrect;
+
+    @Parameter
+    private MeasureService measureService;
+
+    @Override
+    public void run() {
+        measureService.setFiles(Arrays.asList(filesToCorrect));
+        measureService.startMeasureBatch();
+        measureService.nextImage();
+    }
+
+    /** Tests the command. */
+    public static void main(final String... args) throws Exception {
+        // Launch ImageJ for test run.
+        final ImageJ ij = new ImageJ();
+        ij.launch(args);
+        ij.command().run(CorrectionCommand.class, true);
+    }
+}
